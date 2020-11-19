a561b0a2152050c67ddfe753f9c48f9b26e1fe75
南浦月 <nanpuyue@gmail.com>
2020-11-19 20:29:00 +0800

    remove fd.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,25 @@
 use std::convert::TryFrom;
-use std::io::{Error, ErrorKind, Read, Result, Write};
+use std::io::{Error, ErrorKind, Result};
 use std::os::unix::io::{AsRawFd, RawFd};
 use std::pin::Pin;
 use std::task::{Context, Poll, Poll::*};
 
 use tokio::io::{unix, AsyncRead, AsyncWrite, ReadBuf};
 
-use self::fd::Fd;
-
-mod fd;
-
-pub struct AsyncFd(unix::AsyncFd<Fd>);
+pub struct AsyncFd(unix::AsyncFd<RawFd>);
 
 impl TryFrom<RawFd> for AsyncFd {
     type Error = Error;
 
     fn try_from(fd: RawFd) -> Result<Self> {
-        let mut fd = Fd(fd);
-        fd.set_nonblock()?;
+        set_nonblock(fd)?;
         Ok(Self(unix::AsyncFd::new(fd)?))
     }
 }
 
 impl AsRawFd for AsyncFd {
     fn as_raw_fd(&self) -> RawFd {
-        self.0.get_ref().0
+        *self.0.get_ref()
     }
 }
 
@@ -34,42 +29,55 @@ impl AsyncRead for AsyncFd {
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<Result<()>> {
-        let mut fd = Fd(self.as_raw_fd());
         let mut ready = match self.0.poll_read_ready(cx) {
             Ready(x) => x?,
             Pending => return Pending,
         };
 
-        match unsafe {
-            fd.read(&mut *(buf.unfilled_mut() as *mut _ as *mut [u8]))
-                .map(|n| {
-                    buf.assume_init(n);
-                    buf.advance(n);
-                })
-        } {
-            Err(e) if e.kind() == ErrorKind::WouldBlock => {
+        let ret = unsafe {
+            libc::read(
+                self.as_raw_fd(),
+                buf.unfilled_mut() as *mut _ as _,
+                buf.remaining(),
+            )
+        };
+
+        if ret < 0 {
+            let e = Error::last_os_error();
+            if e.kind() == ErrorKind::WouldBlock {
                 ready.clear_ready();
                 Pending
+            } else {
+                Ready(Err(e))
             }
-            x => Ready(x),
+        } else {
+            let n = ret as usize;
+            unsafe { buf.assume_init(n) };
+            buf.advance(n);
+            Ready(Ok(()))
         }
     }
 }
 
 impl AsyncWrite for AsyncFd {
     fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize>> {
-        let mut fd = Fd(self.as_raw_fd());
         let mut ready = match self.0.poll_write_ready(cx) {
             Ready(x) => x?,
             Pending => return Pending,
         };
 
-        match fd.write(buf) {
-            Err(e) if e.kind() == ErrorKind::WouldBlock => {
+        let ret = unsafe { libc::write(self.as_raw_fd(), buf.as_ptr() as _, buf.len()) };
+
+        if ret < 0 {
+            let e = Error::last_os_error();
+            if e.kind() == ErrorKind::WouldBlock {
                 ready.clear_ready();
                 Pending
+            } else {
+                Ready(Err(e))
             }
-            x => Ready(x),
+        } else {
+            Ready(Ok(ret as usize))
         }
     }
 
@@ -79,5 +87,17 @@ impl AsyncWrite for AsyncFd {
 
     fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Result<()>> {
         Ready(Ok(()))
+    }
+}
+
+fn set_nonblock(fd: RawFd) -> Result<()> {
+    let flags = unsafe { libc::fcntl(fd, libc::F_GETFL) };
+    if flags < 0 {
+        return Err(Error::last_os_error());
+    }
+
+    match unsafe { libc::fcntl(fd, libc::F_SETFL, flags | libc::O_NONBLOCK) } {
+        0 => Ok(()),
+        _ => Err(Error::last_os_error()),
     }
 }
